1ca37e9058bec956edd8e6337e09cfbf1a8f988c
julsCadenas <jscadenas12@gmail.com>
2024-07-05 01:34:51 +0800

    fixed css

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,12 +1,23 @@
+import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons'; 
 
-const Search = () => {
-    
+const Search = ({ setSearchTerm }) => {
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value); 
+    };
+
     return (
-        <>
-            <div className="searchContainer">
-                
-            </div>
-        </>    
+        <div className="searchContainer">
+                <input
+                    type="text"
+                    placeholder="Search by title..."
+                    onChange={handleSearchChange} 
+                />
+                {/* <button type="submit">
+                    <FontAwesomeIcon icon={faSearch} />
+                </button> */}
+        </div>
     );
 };
 
--- src/pages/ebooks.jsx
+++ src/pages/ebooks.jsx
@@ -1,73 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, query, where, contains } from 'firebase/firestore';
 import Header from '../components/header';
 import SideBar from '../components/sidebar';
 import Search from '../components/search';
 import Login from './login';
 import { useAuth } from '../contexts/authContext';
-import { collection, getDocs } from "firebase/firestore";
-import React, { useState, useEffect } from 'react';
 import db from '../firebase';
-import BookModal from '../components/bookdetails'; 
+import BookModal from '../components/bookdetails';
 
 const Ebooks = () => {
-  const { userLoggedIn } = useAuth();
-  const [books, setBooks] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [selectedBook, setSelectedBook] = useState(null);
+    const { userLoggedIn } = useAuth();
+    const [books, setBooks] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [selectedBook, setSelectedBook] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const handleOpen = (book) => {
-    setSelectedBook(book);
-    setOpen(true);
-  };
-
-  const handleClose = () => setOpen(false);
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const booksCollection = collection(db, 'Books');
-      const booksSnapshot = await getDocs(booksCollection);
-      const booksList = booksSnapshot.docs.map(doc => doc.data());
-      setBooks(booksList);
+    const handleOpen = (book) => {
+        setSelectedBook(book);
+        setOpen(true);
     };
 
-    fetchBooks();
-  }, []);
+    const handleClose = () => setOpen(false);
 
-  return (
-    <>
-      {userLoggedIn ? (
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const booksCollection = collection(db, 'Books');
+            let queryBooks = query(booksCollection);
+
+            if (searchTerm !== '') {
+                queryBooks = query(booksCollection, where('title', '>=', searchTerm));
+            }
+
+            const booksSnapshot = await getDocs(queryBooks);
+            const booksList = booksSnapshot.docs.map(doc => doc.data());
+            setBooks(booksList);
+        };
+
+        fetchBooks();
+    }, [searchTerm]); 
+
+    return (
         <>
-          <Header />
-          <SideBar />
-          <Search />
-          <div className='ebooksContainer'>
-            <div className='elibbooks' id='ebooks2'>
-              <p className='eBooks'><strong>eBooks</strong></p>
-              <ul>
-                {books.map((book, index) => (
-                  <li key={index} className='bookItem' id='bookitem2'>
-                    <button className="bookbtn" id={book.title} onClick={() => handleOpen(book)}>
-                      <img src={book.cover} alt={`${book.title}`} />
-                      <div className='booktitle'>
-                        <p><strong>{`${book.title}`}</strong></p>
-                      </div>
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+            {userLoggedIn ? (
+                <>
+                    <Header />
+                    <SideBar />
+                    <div className='ebooksContainer'>
+                        <p className='eBooks'><strong>eBooks</strong></p>
+                        <Search setSearchTerm={setSearchTerm} /> 
+                        <div className='ebookscase' id='ebooks2'>
+                            <ul>
+                                {books.map((book, index) => (
+                                    <li key={index} className='books' id='bookitem2'>
+                                        <button className="bookbtn" id={book.title} onClick={() => handleOpen(book)}>
+                                            <img src={book.cover} alt={`${book.title}`} />
+                                            <div className='booktitle'>
+                                                <p><strong>{`${book.title}`}</strong></p>
+                                            </div>
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
 
-          <BookModal 
-            open={open} 
-            handleClose={handleClose} 
-            selectedBook={selectedBook} 
-          />
+                    <BookModal
+                        open={open}
+                        handleClose={handleClose}
+                        selectedBook={selectedBook}
+                    />
+                </>
+            ) : (
+                <Login />
+            )}
         </>
-      ) : (
-        <Login />
-      )}
-    </>
-  );
+    );
 };
 
 export default Ebooks;
