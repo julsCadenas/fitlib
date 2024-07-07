19e51332ddf5154577e1328a09e60d6f4bb78c22
julsCadenas <jscadenas12@gmail.com>
2024-07-07 13:36:13 +0800

    elibrary table component

---
+++ src/components/elibrarytable.jsx
@@ -0,0 +1,97 @@
+import Header from '../components/header';
+import SideBar from '../components/sidebar';
+import Login from '../pages/login';
+import { useAuth } from '../contexts/authContext';
+import React, { useState, useEffect } from 'react';
+import db from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import ElibModal from '../components/admindetails'; 
+import AddBookModal from '../components/addbook'; 
+
+const ElibAdmin = () => {
+    const { userLoggedIn } = useAuth(); 
+    const [books, setBooks] = useState([]);
+    const [openDetails, setOpenDetails] = useState(false);
+    const [openAddBook, setOpenAddBook] = useState(false); 
+    const [selectedBook, setSelectedBook] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const booksCollection = collection(db, 'Library');
+            const booksSnapshot = await getDocs(booksCollection);
+            const booksList = booksSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setBooks(booksList);
+        };
+
+        fetchBooks();
+    }, []); 
+
+    const handleOpenDetails = (book) => {
+        setSelectedBook(book);
+        setOpenDetails(true);
+    };
+
+    const handleCloseDetails = () => setOpenDetails(false);
+
+    const handleOpenAddBook = () => setOpenAddBook(true);
+
+    const handleCloseAddBook = () => setOpenAddBook(false);
+
+    return (
+        <>
+        { userLoggedIn ? 
+            <>
+                <div className='admincontainer'>
+                    <div className='bookstablecontainer'>
+                        <div className='elibraryadmin'>
+                            <p><strong>eLibrary</strong></p>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Book ID</th>
+                                        <th>Title</th>
+                                        <th>Author</th>
+                                        <th>Class</th>
+                                        <th>Status</th>
+                                        <th>Details</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {books.map((book, index) => (
+                                        <tr key={book.id}>
+                                            <td>{book.bookID}</td>
+                                            <td>{book.title}</td>
+                                            <td>{book.author}</td>
+                                            <td>{book.class}</td>
+                                            <td>{book.status}</td>
+                                            <td>
+                                                <button onClick={() => handleOpenDetails(book)}>
+                                                    <strong>Details</strong>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        <button className='addbtn' onClick={handleOpenAddBook}><strong>ADD BOOK</strong></button>
+                    </div>
+                </div> 
+                <ElibModal
+                    open={openDetails}
+                    handleClose={handleCloseDetails}
+                    selectedBook={selectedBook}
+                />
+                <AddBookModal
+                    open={openAddBook}
+                    handleClose={handleCloseAddBook}
+                />
+            </>
+            :
+            <Login />
+        }
+        </>
+    );
+}
+
+export default ElibAdmin;
--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -7,35 +7,10 @@ import db from '../firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import ElibModal from '../components/admindetails'; 
 import AddBookModal from '../components/addbook'; 
+import ElibAdmin from '../components/elibrarytable';
 
 const Admin = () => {
     const { userLoggedIn } = useAuth(); 
-    const [books, setBooks] = useState([]);
-    const [openDetails, setOpenDetails] = useState(false);
-    const [openAddBook, setOpenAddBook] = useState(false); 
-    const [selectedBook, setSelectedBook] = useState(null);
-
-    useEffect(() => {
-        const fetchBooks = async () => {
-            const booksCollection = collection(db, 'Library');
-            const booksSnapshot = await getDocs(booksCollection);
-            const booksList = booksSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            setBooks(booksList);
-        };
-
-        fetchBooks();
-    }, []); 
-
-    const handleOpenDetails = (book) => {
-        setSelectedBook(book);
-        setOpenDetails(true);
-    };
-
-    const handleCloseDetails = () => setOpenDetails(false);
-
-    const handleOpenAddBook = () => setOpenAddBook(true);
-
-    const handleCloseAddBook = () => setOpenAddBook(false);
 
     return (
         <>
@@ -45,49 +20,9 @@ const Admin = () => {
                 <SideBar />
                 <div className='admincontainer'>
                     <div className='bookstablecontainer'>
-                        <div className='elibraryadmin'>
-                            <p><strong>eLibrary</strong></p>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Book ID</th>
-                                        <th>Title</th>
-                                        <th>Author</th>
-                                        <th>Class</th>
-                                        <th>Status</th>
-                                        <th>Details</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {books.map((book, index) => (
-                                        <tr key={book.id}>
-                                            <td>{book.bookID}</td>
-                                            <td>{book.title}</td>
-                                            <td>{book.author}</td>
-                                            <td>{book.class}</td>
-                                            <td>{book.status}</td>
-                                            <td>
-                                                <button onClick={() => handleOpenDetails(book)}>
-                                                    <strong>Details</strong>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                        <button className='addbtn' onClick={handleOpenAddBook}><strong>ADD BOOK</strong></button>
+                        <ElibAdmin />
                     </div>
                 </div> 
-                <ElibModal
-                    open={openDetails}
-                    handleClose={handleCloseDetails}
-                    selectedBook={selectedBook}
-                />
-                <AddBookModal
-                    open={openAddBook}
-                    handleClose={handleCloseAddBook}
-                />
             </>
             :
             <Login />
